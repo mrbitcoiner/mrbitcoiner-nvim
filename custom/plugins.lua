158c76ec58f8e96ca0abd85567eba4289d647aba
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-04-25 12:32:53 +0000

    Added scrollof 8

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -13,6 +13,6 @@ local plugins = {
       require("plugins.configs.lspconfig")
       require("custom.configs.lspconfig")
     end,
-  }
+  },
 }
 return plugins
--- custom/set.lua
+++ custom/set.lua
@@ -2,3 +2,5 @@
 vim.opt.number = true
 -- relative number line
 vim.opt.relativenumber = true
+-- scroll to keep 8 lines from cursor top / bottom
+vim.opt.scrolloff = 8
