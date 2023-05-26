cfdd861291a427af051271c59ebd3df396e375ee
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-05-26 19:40:49 +0000

    * Rustfmt   - Added rustfmt   - Auto format on save

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4,16 +4,29 @@ local plugins = {
     opts = {
       ensure_installed = {
         "rust-analyzer",
-	"lua-language-server",
+      	"lua-language-server",
       },
     },
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+      require ("custom.configs.null-ls")
+     end,
+    },
     config = function()
       require("plugins.configs.lspconfig")
       require("custom.configs.lspconfig")
     end,
   },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  }
 }
 return plugins
