bfea4abbe33a0e948e6b15f0f4662c2e59a11ea3
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-05-29 18:15:26 +0000

    * Removed shell script lint from config (since is not installed)

--- custom/configs/null-ls.lua
+++ custom/configs/null-ls.lua
@@ -1,11 +1,9 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
-local lint = null_ls.builtins.diagnostics
 
 local sources = {
    formatting.rustfmt,
-   lint.shellcheck,
 }
 
 null_ls.setup {
